1ccf5aed34acb0301199a9315990bcb24b9696da
Laurent MC <lmc@outlook.fr>
2022-02-12 18:46:29 +0300

    Commit du soir

---
+++ Test_08_Dir.py
@@ -0,0 +1,8 @@
+import random
+from pprint import pprint
+
+pprint(dir(random))
+
+#pprint(help(random.randint))
+# pprint(dir(random))
+
---
+++ Test_09_List.py
@@ -0,0 +1,36 @@
+# Récupérez le premier et le dernier nombre contenus dans cette liste dans les variables 'nombre_premier' et 'nombre_dernier'.
+nombres = [1, 2, 3, 4, 5, 4, 3, 2, 1]
+nombre_premier = nombres[0] 
+nombre_dernier = nombres[-1]
+
+# Récupérer l'élément 'Python' contenu dans la liste dans la variable 'langage'.
+langages = ["Java", "Python", "C++"]
+langage = langages[1]
+
+# Changez la position de l'élément 'Python' dans la liste pour qu'il se retrouve à la fin de la liste (["Java", "C++", "Python"])
+liste = ["Java", "Python", "C++"]
+liste.remove('Python')
+liste.append('Python')
+# print(liste)
+
+usrer_list = ['Utilisateur_01',
+        'Utilisateur_02',
+        'Utilisateur_03',
+        'Utilisateur_04',
+        'Utilisateur_05',
+        'Utilisateur_06']
+
+#print(usrer_list[:4])
+
+liste = ["Maxime", "Martine", "Christopher", "Carlos", "Michael", "Eric"]
+trois_premiers = liste[:3]
+print(trois_premiers)
+trois_derniers = liste[3:]
+print(trois_derniers)
+milieu = liste[1:-1]
+print(milieu)
+premier_dernier = liste[::5]
+print(premier_dernier)
+
+pos_carlos = liste.index("Carlos")
+print(f"Carlos est arrivé en position {pos_carlos}.")
